0f3ebcb4c406dc6cd32951dc0034825b644a851a
Nathan-Etave <nathan.etave@gmail.com>
2024-01-31 16:59:57 +0100

    feat/ Implémentation du routeur

---
+++ app/Router.php
@@ -0,0 +1,48 @@
+<?php
+namespace app;
+
+class Router
+{
+    protected $routes = [
+        'GET' => [],
+        'POST' => []
+    ];
+
+    public function get($uri, $controller)
+    {
+        $this->routes['GET'][$uri] = $controller;
+    }
+
+    public function post($uri, $controller)
+    {
+        $this->routes['POST'][$uri] = $controller;
+    }
+    
+    public function run()
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        $uri = $_SERVER['REQUEST_URI'];
+        $uri_explode = explode('/', $_SERVER['REQUEST_URI']);
+        foreach ($uri_explode as $key => $elem) {
+            if (is_numeric($elem)) {
+                $uri_explode[$key] = '{id}';
+            }
+        }
+        $uri_implode = implode('/', $uri_explode);
+        $found = false;
+        foreach ($this->routes[$method] as $route => $_) {
+            if ($route === $uri_implode) {
+                $uri_explode = explode('/', $uri);
+                $id = end($uri_explode);
+                call_user_func_array($this->routes[$method][$uri_implode], [$id]);
+                $found = true;
+                break;
+            }
+        }
+        if (!$found) {
+            header("HTTP/1.0 404 Not Found");
+            echo '<h1>404 Not Found</h1>';
+        }
+    }
+}
+?>
