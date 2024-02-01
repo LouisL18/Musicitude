5ea8f2366c0c2d22ff8f9cea7cc04aeb20ef6a99
Nathan-Etave <nathan.etave@gmail.com>
2024-02-01 09:05:56 +0100

    fix/ Fix des controlleurs dans le RoutesLoader

--- provider/Routesloader.php
+++ provider/Routesloader.php
@@ -13,14 +13,13 @@ use app\Controllers\ArtistController;
 use app\Factories\UserFactory;
 use app\Controllers\UserController;
 
-$albumController = new AlbumController(new AlbumFactory(DatabaseProvider::getDataBase()));
-$artistController = new ArtistController(new ArtistFactory(DatabaseProvider::getDataBase()));
-$userController = new UserController(new UserFactory(DatabaseProvider::getDataBase()));
+$albumController = new AlbumController(new AlbumFactory(DatabaseProvider::getDataBase()), new ArtistFactory(DatabaseProvider::getDataBase()), new UserFactory(DatabaseProvider::getDataBase()));
+$artistController = new ArtistController(new ArtistFactory(DatabaseProvider::getDataBase()), new AlbumFactory(DatabaseProvider::getDataBase()), new UserFactory(DatabaseProvider::getDataBase()));
+$userController = new UserController(new UserFactory(DatabaseProvider::getDataBase()), new ArtistFactory(DatabaseProvider::getDataBase()));
 
 $router = new Router();
 
 $router->get('/', [$userController, 'login']);
-$router->post('/login', [$userController, 'login']);
 $router->get('/register', [$userController, 'register']);
 $router->get('/albums', [$albumController, 'index']);
 $router->get('/album/{id}', [$albumController, 'detail']);
@@ -37,6 +36,7 @@ $router->delete('/album/{id}', [$albumController, 'delete']);
 $router->delete('/artist/{id}', [$artistController, 'delete']);
 $router->delete('/user/{id}', [$userController, 'delete']);
 
+$router->post('/login', [$userController, 'login']);
 $router->post('/album', [$albumController, 'create']);
 $router->post('/artist', [$artistController, 'create']);
 $router->post('/user', [$userController, 'create']);
