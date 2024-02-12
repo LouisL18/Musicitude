28f668d1f430e1f5dbf42150b229f91a04710879
RomainMechain <romain.mechain@etu.univ-orleans.fr>
2024-02-12 15:30:52 +0100

    Feat/ Ajout des données pour l'index des playlists

--- app/Controllers/PlaylistController.php
+++ app/Controllers/PlaylistController.php
@@ -2,27 +2,28 @@
 namespace app\Controllers;
 
 use app\Factories\AlbumFactory;
-use app\Factories\ArtistFactory;
 use app\Factories\UserFactory;
 use app\Factories\PlaylistFactory;
 class PlaylistController {
     protected $playlistFactory;
     protected $userFactory;
-    protected $musiqueFactory;
+    protected $albumFactory;
 
-    public function __construct(PlaylistFactory $playlistFactory, UserFactory $userFactory, MusiqueFactory $musiqueFactory) {
+    public function __construct(PlaylistFactory $playlistFactory, UserFactory $userFactory, AlbumFactory $albumFactory) {
         $this->playlistFactory = $playlistFactory;
         $this->userFactory = $userFactory;
-        $this->musiqueFactory = $musiqueFactory;
+        $this->albumFactory = $albumFactory;
     }
 
     public function index() {
         $playlists = $this->playlistFactory->getAllPlaylists();
         $super_playlists = [];
         foreach ($playlists as $playlist) {
+            $musique1 = $this->playlistFactory->getMusiquesByPlaylist($playlist->getIdPlaylist())[0];
             $super_playlists[] = [
                 'Playlist' => $playlist,
-                'Musiques' => $this->playlistFactory->getMusiquesByPlaylist($playlist->getIdPlaylist()),
+                'NbMusiques' => count($this->playlistFactory->getMusiquesByPlaylist($playlist->getIdPlaylist())),
+                'Image' => $this->albumFactory->getImageById($musique1->getIdImage()),
             ];
         }
         global $main;
